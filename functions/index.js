e4550b5650279c9c8ce6f11c8ae8080db66a823f
SantosLaprida <lapridasantos95@gmail.com>
2025-03-07 14:00:28 -0700

    Error 404 fixed

--- functions/index.js
+++ functions/index.js
@@ -12,37 +12,43 @@
 // const functions = require("firebase-functions/v2");
 const {onSchedule} = require("firebase-functions/v2/scheduler");
 const admin = require("firebase-admin");
+
+admin.initializeApp();
+
 const {getFirestore} = require("firebase-admin/firestore");
 const {fetchRankings} = require("./api/golfApi");
 // const {initializeApp} = require("firebase-admin/app");
 
-admin.initializeApp();
-
 const db = getFirestore();
 
-exports.updateRankings = onSchedule("every day 01:30", async (event) => {
+
+exports.updateRankings = onSchedule({
+  schedule: "every sunday 02:30",
+}, async (event) => {
   try {
-    const year = "2025";
+    const date = new Date();
+    const year = date.getFullYear();
     const statId = "186";
 
     const rankings = await fetchRankings(year, statId);
 
     for (const player of rankings) {
-      const playerDocRef = db.collection("I_MaestroJugadores").
-          doc(player.id);
-      await playerDocRef.update({
-        lastname: player.lastname,
-        firstname: player.firstname,
-        name: player.firstname + " " + player.lastname,
+      const playerDocRef = db.collection("I_MaestroJugadores")
+          .doc(player.playerId);
+      await playerDocRef.set({
+        lastname: player.lastName,
+        firstname: player.firstName,
+        name: player.fullName,
         ranking: player.rank,
-      });
+      }, {merge: true});
     }
-    console.log("Rankings updated successfully");
+    console.log("Rankings updated successfully!");
+    return null;
   } catch (error) {
     console.error("Error fetching or updating rankings", error);
+    return null;
   }
 });
-
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
